4b7e762f80a6f8b9dd2cb7748e4bfb529e41923f
Jalajkala <41810833+Jalajkala@users.noreply.github.com>
2018-07-28 08:27:46 +0530

    Initial commit

---
+++ Test.ps1
@@ -0,0 +1,15 @@
+﻿[int]$num1 = Read-Host "Enter first number"
+[int]$num2 = Read-Host "Enter second number"
+[int]$num3 = Read-Host "Enter third number"
+
+if ($num1 -gt $num2)
+{
+    if ($num1 -gt $num3)
+    {
+        write-host "$num1 is largest of all !!"
+    }else{write-host "$num3 is largest of all !!"}
+}
+elseif ($num2 -gt $num3)
+{
+    write-host "$num2 is greatest of all !!"
+}else {write-host "$num3 is greatest of all !!"}
